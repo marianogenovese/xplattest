350926ed66ec1af1699407af38883cbba797e129
Mariano <mgenovese@iid.ec>
2018-10-29 17:09:37 -0500

    Initial commit

---
+++ SharedProject1/Class1.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SharedProject1
+{
+    [TestFixture]
+    class ExampleTests
+    {
+        [Test]
+        public void SmokeTest()
+        {
+            Assert.That(true);
+        }
+    }
+}
